1b1c5200f120b46b593443315c9a38a0a494b872
um-31 <udaymahajan97@gmail.com>
2020-05-09 21:31:21 -0400

    first commit

--- Wordgame/BasicViewController.swift
+++ Wordgame/BasicViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var WordgameLable: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
